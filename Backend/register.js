776013fd106aaaeaa713c29451c1cee49cc38561
inumaka <21110025@oic-ok.ac.jp>
2023-06-09 15:37:14 +0900

    v2

--- Backend/register.js
+++ Backend/register.js
@@ -1,9 +1,13 @@
 
+const accepts = require('accepts');
 const e = require('express');
 var http = require('http');
 var html = require('fs').readFileSync('../designDictionary/html/register.html');
 var Account = [];
 var id =0;
+// var a = 0;
+// var b = 0;
+// var c = 0;
 
 http.createServer(function(req, res){
     var data = '';
@@ -25,7 +29,7 @@ http.createServer(function(req, res){
                               console.log("成功");
                             if(data[j] === "&" || j == data.length){
                                 flag = false
-                                Account[x] = [data.substring(i,j)]
+                                Account[x] = data.substring(i,j);
                                 x++;
                                 break;
                             }
@@ -40,7 +44,9 @@ http.createServer(function(req, res){
                 }
                 console.log("変数dataの値は" + data);
                 console.log(Account[0],Account[1],Account[2],Account[3]);
-                
+                // var a ="a";
+                // var b = Account[1];
+                // var c = Account[3];
                 InsData(Account);
             })
             .on('end', function(){
@@ -51,7 +57,7 @@ http.createServer(function(req, res){
     }
 }).listen(8080);
 
-function InsData(Account){
+function InsData(Account,res){
     
     const {Client} = require("pg");
 const client = new Client({
@@ -61,17 +67,21 @@ const client = new Client({
     password: "shirokuma123",//ユーザーパスワード
     port: 5432, 
 });
-client.connect();
-const query = {
-    text: 'INSERT INTO user_info(user_name, pwd, address) SELECT $3,$2,CAST($1 AS VARCHAR) WHERE NOT EXISTS (SELECT 1 FROM user_info WHERE address = $1)',
-    values: [Account[0],Account[1],Account[3]],
-};
+client.connect()
+    .then(() => {
+      const query = {
+        text: 'INSERT INTO user_info (user_name ,pwd, address ) VALUES ($3, $2, $1);',
+        values: [Account[0], Account[1], Account[3]],
+      };
 
-client
-    .query(query)
-    .then((res) => {
-        console.log(res);
-        client.end();
+      return client.query(query);
     })
-    .catch((e) => console.error(e.stack));
+    .then((res) => {
+      console.log(res);
+      client.end();
+    })
+    .catch((e) => {
+      console.error(e.stack);
+      client.end();
+    });
 }
