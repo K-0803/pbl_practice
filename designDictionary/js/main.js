7a8ca3cb800955be10f546aff668404d4d0297e9
Vegey <119383194+Vegey@users.noreply.github.com>
2023-06-13 09:42:07 +0900

    main.js　ハンバーガーメニュー追加

--- designDictionary/js/main.js
+++ designDictionary/js/main.js
@@ -1,3 +1,11 @@
 $(".openbtn").click(function () {
-    $(this).toggleClass('active');
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションリンククリック
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
+    $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
 });
