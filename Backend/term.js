b311824943babbc1a76079209032bbc44a79016a
K-0803 <108750099+K-0803@users.noreply.github.com>
2023-06-15 11:30:00 +0900

    Delete 神経衰弱.html

--- Backend/term.js
+++ Backend/term.js
@@ -2,6 +2,17 @@ const http = require('http');
 const { Pool } = require('pg');
 const fs = require('fs');
 
+const path = require('path');   //相対パスを使用可能にする
+const bodyParser = require('body-parser');  //req.bodyを使用できるようにする
+const express = require('express');
+const app = express();
+
+//ミドルウェアの設定
+app.use(bodyParser.urlencoded({ extended: false }));    //req.bodyを使用するためのミドルウェア
+app.use(bodyParser.json());                             //
+app.use(express.static(path.join('../designDictionary/html'))); //相対パスを使用するためのミドルウェア
+app.use(express.json());
+
 // データベースの接続情報
 const dbConfig = {
     user: "postgres",//ユーザー名
@@ -10,13 +21,19 @@ const dbConfig = {
     password: "shirokuma123",//ユーザーパスワード
     port: 5432, 
 };
-
 // データベース接続
 const pool = new Pool(dbConfig);
 
-// HTTPサーバーの作成
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
+//getリクエストの処理
+app.get('/siteURL', function(req, res){
+  //ページが開かれたときにhtmlを表示する処理
+  const filePath = path.resolve(__dirname + './../designDictionary/html/searchResult.html');
+  // console.log(filePath);
+  // res.sendFile(filePath);
+
+  const search = req.body;//リクエストボディのデータ取得
+  //if文の条件を変更。キーワード入力エリアが空白でない場合に処理実行
+  if (search != '') {
     // データベースからデータを取得するクエリ
     const query = 'SELECT * FROM css_model';
 
@@ -59,6 +76,58 @@ const server = http.createServer((req, res) => {
 });
 
 // サーバーの起動
-server.listen(8080, () => {
+app.listen(8080, () => {
   console.log('サーバーがポート8080で起動しました');
 });
+
+
+
+
+// HTTPサーバーの作成
+// const server = http.createServer((req, res) => {
+//   if (req.url === '/') {
+//     // データベースからデータを取得するクエリ
+//     const query = 'SELECT * FROM css_model';
+
+//     // クエリを実行し、結果を取得
+//     pool.query(query, (err, result) => {
+//       if (err) {
+//         console.error('クエリエラー:', err);
+//         res.statusCode = 500;
+//         res.end('データベースエラーが発生しました');
+//       } else {
+//         fs.readFile('../designDictionary/html/searchResult.html', 'utf8', (err, htmlContent) => {
+//           if (err) {
+//             console.error('HTML読み込みエラー:', err);
+//             res.statusCode = 500;
+//             res.end('HTMLファイルの読み込みエラーが発生しました');
+//           } else {
+//             // HTMLテーブルの作成
+//             let tableHTML = '<table>';
+//             tableHTML += '<tr><th>カラーコード</th><th>説明</th></tr>';
+
+//             result.rows.forEach((row) => {
+//               tableHTML += `<tr><td>${row.css_code}</td><td>${row.css_summary}</td></tr>`;
+//             });
+
+//             tableHTML += '</table>';
+
+//             // レスポンスとしてHTMLを返す
+//             htmlContent = htmlContent.replace('{{table}}', tableHTML);
+//             res.setHeader('Content-Type', 'text/html');
+//             res.statusCode = 200;
+//             res.end(htmlContent);
+//           }
+//         });
+//       }
+//     });
+//   } else {
+//     res.statusCode = 404;
+//     res.end('Not Found');
+//   }
+// });
+
+// // サーバーの起動
+// server.listen(8080, () => {
+//   console.log('サーバーがポート8080で起動しました');
+// });
