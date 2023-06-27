f472029744a557015dee82833a778d88e77d2b5f
Frandremilk <manianokatamari@gmail.com>
2023-06-27 10:20:23 +0900

    試行錯誤

--- Backend/loginProcess.js
+++ Backend/loginProcess.js
@@ -10,12 +10,13 @@ const port = 80;
 var html = require('fs').readFileSync('../designDictionary/html/login.html');
 var resultArray = [];
 var resultCnt = 0;
+var resultId = 0;
 // var reditFlag = false;
 
 //ミドルウェアの設定
 app.use(bodyParser.urlencoded({ extended: false }));    //req.bodyを使用するためのミドルウェア
 app.use(bodyParser.json());                             //
-app.use(express.static(path.join('../designDictionary'))); //相対パスを使用するためのミドルウェア
+app.use(express.static(path.join('../designDictionary'))); 
 app.use(express.json());
 
 //getリクエストの処理・ページを開いたときにhtmlが表示される
@@ -49,72 +50,71 @@ app.post('/login', function(req, res){
     
 })
 
-// app.listen(port,'107.22.226.32' ,()=>{
-    app.listen(8080, function(){
 
-    console.log('サーバーがポート8080で起動しました。');
+// app.listen(port,'107.22.226.32' ,()=>{
+//     console.log('サーバーが起動しました。');
+// })
+
+app.listen(8080, function(){
+    console.log("aaaaa!");
 })
 
+function getPass(email, pass){
+    const {Client} = require("pg");
+    const client = new Client({
+        user: "postgres",//ユーザー名
+        host: "database-2.cgz0heptpctb.us-east-1.rds.amazonaws.com",//ホスト
+        database: "postgres",//DB名
+        password: "shirokuma123",//ユーザーパスワード
+        port: 5432, 
+    });
 
-// http.createServer(function(req, res){
-//     var data = '';
-//     var data_test = [];
-//     var flag = false;
-//     var x = 0;
-//     // var result = '';
+    return new Promise(function(resolve, reject){
+        client
+            .connect()
+            .then(function(){
+                const query = {
+                    text: "SELECT user_id, user_name from user_info where address = ($1) and pwd = ($2)",
+                    values: [email, pass],
+                };
 
-//     if(req.method === 'GET'){
-//         res.writeHead(200, {'Content-Type' : 'text/html'});
-//         res.end(html);
-//     }else if(req.method === 'POST'){    
-//         req.on('data',function(chunk){
-//             data += chunk;
-//             console.log(data);
-//             for(var i = 0;i < data.length;i++){
-//                 if(flag === true){
-//                     for(var j = i;j <= data.length;j++){
-//                         if(data[j] === "&" || j == data.length){
-//                             flag = false;
-//                             data_test[x] = data.substring(i,j);
-//                             console.log("変数dataの値は!" + data_test[x] +",xの中身は" + x);
-//                             x++;
-//                             break;
-//                         }
-//                     }
-//                 }
-//                 if(data[i] === "="){
-//                     flag = true;
-//                     continue;
-//                 }
-
-//             }
-
-//             getPass(data_test[0], data_test[1])
-//                 .then(function(redUrl){
-//                     console.log('値は=' + redUrl);
-//                     if (redUrl === '/redirect') {
-//                         res.writeHeader(302, { Location: '../designDictionary/html/index.html' });
-//                         res.end();
-//                     } else {
-//                         res.end(html);
-//                     }
-//                     })
-//                 .catch((error) => {
-//                     console.error(error);
-//                     res.end(html);
-//                 });
                 
-
-//         })
+                return client.query(query);
+            })
+            .then(function(res){
+                // console.log(res);
+                resultCnt = res.rowCount;
+                if(resultCnt == 0){
+                    console.log("resultCnt = 0");
+                    redct = 'error';
+                    notifier.notify({
+                        title: "エラー通知",
+                        message:"入力ミスがあります。再入力して下さい。"
+                    });
+                }else{
+                    resultArray = res.rows[0].user_name;
+                    resultId = res.rows[0].user_id;
+                    console.log("resultCnt != 0")
+                    redct = '/redirect';
+                    notifier.notify({
+                        title: "ログイン通知",
+                        message:`ようこそ${resultArray}さん`
+                    });
+                    
+                }
+                console.log(resultArray);
+                console.log(resultId);
+                console.log(redct);
+                client.end();
+                resolve(redct, resultId);
+            })
+            .catch(function(e){
+                console.error(e.stack);
+                reject(e);
+            });
+    });
     
-//            .on('end', function(){
-//             console.log('受け取り完了');
-//             res.end(html);
-//         })
-//     }
-// }).listen(8080,function(){
-//     console.log("server running!");
-// });
+}
 
 function getPass(email, pass){
     const {Client} = require("pg");
--- Backend/webCreate.js
+++ Backend/webCreate.js
@@ -1,10 +1,11 @@
-
-var http = require('http');
-var html = require('fs').readFileSync('../designDictionary/html/webCreate.html');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
+const fs = require('fs');
+const {v4: uuidv4} = require('uuid');
+const {Client} = require("pg");
+const cookieParser = require('cookie-parser');
 // const router = require('../designDictionary/html/login.html');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,7 +13,10 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../designDictionary/html')));
 app.use(express.json());
 
-app.get('/register', function(req, res){
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.get('/', function(req, res){
   const filePath = path.join('../designDictionary/html/webCreate.html');
   console.log(filePath);
   res.sendFile(filePath);
@@ -20,40 +24,50 @@ app.get('/register', function(req, res){
   res.end();
 });
 
-  app.post('/register', function(req, res){
-//     const {email, pass, name} = req.body;
-//     console.log(email);
-//     console.log(pass);
-//     console.log(name);
-//     InsData(email, pass , name )
-//       .then(function(redUrl){
-//         console.log('値は=' + redUrl);
-//         if (redUrl === '/redirect') {
-//             res.redirect(req.baseUrl + '/login.html');
-//             res.end();
-           
-//         } else {
-//             res.end(html);
-//         }
-//         })
-//     .catch((error) => {
-//         console.error(error);
-//         res.end(html);
-//     });
+app.post('/generate', function(req, res){
+  //cookieに保存されているuserIdを取得
+  const userId = req.cookies.userId;
+  //デバッグ用userId
+  // const userId = 1;
+
+  //入力データの取得
+  const {htmlContent, cssContent, comment} = req.body;
+
+  //ファイル名を一意なIDとして生成
+  const fileId = uuidv4();
+
+  //HTMLファイルとCSSファイルを生成して保存
+  const htmlFilePath = `../saveFile/html/${fileId}.html`;
+  const cssFilePath = `../saveFile/css/${fileId}.css`;
+  fs.writeFileSync(htmlFilePath, htmlContent);
+  fs.writeFileSync(cssFilePath, cssContent);
+
+  
+  const client = new Client({
+      user: "postgres",//ユーザー名
+      host: "database-2.cgz0heptpctb.us-east-1.rds.amazonaws.com",//ホスト
+      database: "postgres",//DB名
+      password: "shirokuma123",//ユーザーパスワード
+      port: 5432, 
+  });
+  client.connect();
+  const query = {
+    text: "INSERT INTO web_create (user_id, ccss_code, chtml_code, write_comment) VALUES ($1, $2, $3, $4)",
+    values: [userId, fileId, fileId, comment],
+  };
+  client.query(query, function(error, result){
+    if(error){
+      console.error('データベースエラー：', error);
+      // res.status(500).send('データベースエラーが発生しました');
+      res.redirect(req.baseUrl + '/html/index.html');
+    }else{
+      // res.send('ファイルが生成され、データベースに保存されました');
+      res.redirect(req.baseUrl + '/html/webCreate.html');
+    }
+  });
 
 })
+
 app.listen(8080, function(){
   console.log('サーバーがポート8080で起動しました。');
 })
-
-// function InsData(email, pass , name ) {
-// //postgres接続
-//   const { Client } = require("pg");
-//   const client = new Client({
-//     user: "postgres",//ユーザー名
-//     host: "database-2.cgz0heptpctb.us-east-1.rds.amazonaws.com",//ホスト
-//     database: "postgres",//DB名
-//     password: "shirokuma123",//ユーザーパスワード
-//     port: 5432,
-//   });
-// }
