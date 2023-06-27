adc4f86e08093e32c15d2cd80766662e4876f62f
ひか <119379877+inumaka@users.noreply.github.com>
2023-06-27 13:29:43 +0900

    ejs register

--- Backend/register.js
+++ Backend/register.js
@@ -1,89 +1,91 @@
 
-var http = require('http');
-var html = require('fs').readFileSync('../designDictionary/html/register.html');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
-const resultArray = [];
-// const router = require('../designDictionary/html/login.html');
+
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../designDictionary')));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../designDictionary')));
 
-app.get('/register', function(req, res){
-  const filePath = path.join('../designDictionary/html/register.html');
-  console.log(filePath);
-  res.sendFile(filePath);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../designDictionary/html/views'));
 
-  res.end();
+app.get('/register', function(req, res) {
+  res.render('register', { mail: "", pass: "" , repass: "" }); 
 });
 
-  app.post('/register', function(req, res){
-    const {email, pass, name} = req.body;
-    console.log(email);
-    console.log(pass);
-    console.log(name);
-    InsData(email, pass , name )
-      .then(function(redUrl){
-        console.log('値は=' + redUrl);
-        if (redUrl === '/redirect') {
-            res.redirect(req.baseUrl + '/login.html');
-            res.end();
-           
-        } else {
-            res.end(html);
-        }
-        })
+app.post('/register', function(req, res) {
+  const { email, pass, name ,boolean1 , boolean2} = req.body;
+  console.log(email);
+  console.log(pass);
+  console.log(name);
+  console.log(boolean1);
+  console.log(boolean2);
+  InsData(email, pass, name)
+    .then(function(redUrl) {
+      console.log('値は=' + redUrl);
+      if (redUrl === '/redirect') {
+        res.redirect(req.baseUrl + '/login.html');
+      } else {
+        res.render('register'); 
+      }
+    })
     .catch((error) => {
-      console.log("functionError");
-        console.error(error);
-        res.render('register', { error });
+
+    const inputerr = {
+       mail: error.code,
+       pass: boolean1,
+       repass: boolean2
+    };
+      
+      if(error.code==23505){
+        console.error(error.code);
+      res.render('register', inputerr);
+      }
     });
+});
 
-})
-app.listen(8080, function(){
+app.listen(8080, function() {
   console.log('サーバーがポート8080で起動しました。');
-})
+});
 
-function InsData(email, pass , name ) {
-//postgres接続
-  const { Client } = require("pg");
+function InsData(email, pass, name) {
+  // postgres接続
+  const { Client } = require('pg');
   const client = new Client({
-    user: "postgres",//ユーザー名
-    host: "database-2.cgz0heptpctb.us-east-1.rds.amazonaws.com",//ホスト
-    database: "postgres",//DB名
-    password: "shirokuma123",//ユーザーパスワード
+    user: 'postgres', // ユーザー名
+    host: 'database-2.cgz0heptpctb.us-east-1.rds.amazonaws.com', // ホスト
+    database: 'postgres', // DB名
+    password: 'shirokuma123', // ユーザーパスワード
     port: 5432,
   });
-  //データベース追加
-  return new Promise(function(resolve, reject){
+
+  // データベース追加
+  return new Promise(function(resolve, reject) {
     client
-        .connect()
-        .then(function(){
-            const query = {
-          text: 'INSERT INTO user_info (user_name ,pwd, address ) VALUES ($3, $2, $1);',
-          values: [email, pass , name],
+      .connect()
+      .then(function() {
+        const query = {
+          text: 'INSERT INTO user_info (user_name, pwd, address) VALUES ($3, $2, $1);',
+          values: [email, pass, name],
         };
         return client.query(query);
       })
-      .then(function(res){
-          
-          resultCnt = res.rowCount;
-          redct='/redirect';
-          console.log(resultArray);
-          console.log(redct);
-          client.end();
-          resolve(redct);
+      .then(function(res) {
+        resultCnt = res.rowCount; 
+        redct = '/redirect';
+        console.log(redct);
+        client.end();
+        resolve(redct);
       })
-      .catch(function(e){
-        console.log("SQLerror");
-          console.error(e.stack);
-          reject(e);
-      
+      .catch(function(e) {
+        console.log('SQLerror');
+        console.error(e.stack);
+        reject(e);
       });
-});
-
+  });
 }
