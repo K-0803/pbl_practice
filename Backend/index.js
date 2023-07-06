1774432b4cc9c8ab0bc92716dc83bf734ba695f3
kaito4562 <21110004@oic-ok.ac.jp>
2023-07-06 10:54:53 +0900

    index

---
+++ Backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+app.use(express.static('public'));
+
+// ホームページの表示
+router.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+
+module.exports =router;
