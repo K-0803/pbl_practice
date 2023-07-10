5f617b1d016cd5f1a4807b09b5d692264060ab26
kaito4562 <21110004@oic-ok.ac.jp>
2023-07-10 10:13:48 +0900

    k

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 
 const app = express();
 
@@ -9,13 +10,18 @@ app.use(express.urlencoded({
 }));
 
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../designDictionary/html/views'));
+
+
+
 // Node.jsファイル1
 app.use('/', require('./index')); //indexページ遷移
 app.use('/search', require('./term'));  //検索結果ページ遷移
 app.use('/termDictionary', require('./termDictionary'));  //用語辞典ページ遷移
 app.use('/generate', require('./webCreate'));  //ウェブ作成ページ遷移
 app.use('/favorite', require('./favorite'));  //お気に入りページ遷移
-app.use('/regiater', require('./register'));  //新規登録ページ遷移
+app.use('/register', require('./register'));  //新規登録ページ遷移
 app.use('/login', require('./loginProcess'));  //ログインページ遷移
 app.use('/customList', require('./customList'));  //カスタムリストページ遷移
 app.use('/Wtemplate', require('./Wtemplate'));  //webテンプレートページ遷移
