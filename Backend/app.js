5d1bcc9617bd9b09578b5440d6f7daf325ca43e2
ひか <119379877+inumaka@users.noreply.github.com>
2023-07-21 10:57:18 +0900

    register

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require("path");
 const app = express();
-const path = require('path');
 const notifier = require('node-notifier');
 
 app.use(express.static(path.join(__dirname, '../designDictionary')));
